5281a9890b74c711da77b245d11ac4b729bb8ae1
yabogdanov <dante4645@yandex.ru>
2015-04-19 16:51:02 +0400

    reconfigure pom add models

---
+++ src/main/java/ru/wonderkot/comicsChange/model/Author.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package ru.wonderkot.comicsChange.model;
+
+/**
+ * @author Wonderkot
+ *
+ */
+public class Author {
+	private Long id;
+	private String name;
+	private String surname;
+	private String patronymic;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getPatronymic() {
+		return patronymic;
+	}
+	public void setPatronymic(String patronymic) {
+		this.patronymic = patronymic;
+	}
+}
